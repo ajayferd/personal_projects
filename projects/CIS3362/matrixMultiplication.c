dac45b69d41e9a8e4871b41a356da5503742e1c8
Alejandro Fernandez <alejandro.fernandez@knights.ucf.edu>
2022-10-12 18:32:20 -0400

    added folder for javascript

---
+++ projects/CIS3362/matrixMultiplication.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void matMult(int mat1[][10], int mat2[][10], int mat3[][10]);
+
+int main()
+{
+    int matrix1[][10] = {
+        {}
+        {}
+        {}
+        {}
+        ...
+    };
+    int matrix2[][10];
+    int matrix[][10];
+}
+
+void matMult(int mat1[][10], int mat2[][10], int mat3[][10])
+{
+    // length of mat1
+    mat3 += mat1[i][j] * mat2[i][j]
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            mat3[i][j] = mat
+        }
+    }
+}
