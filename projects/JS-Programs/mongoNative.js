42e1a64103fa4938eb972959317f44588996d9aa
ajayferd <alejandro.fernandez@knights.ucf.edu>
2022-11-07 18:43:13 -0500

    updated files

--- projects/JS-Programs/mongoNative.js
+++ projects/JS-Programs/mongoNative.js
@@ -1,31 +1,23 @@
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb+srv://admin:JxXkJqxYxLMcfhhF@fernandeztestings.jisorjh.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:JxXkJqxYxLMcfhhF@fernandeztestings.jisorjh.mongodb.net/test";
+const client = new MongoClient(uri);
 
-    const client = new MongoClient(uri);
+try {
+    // Connect to the MongoDB cluster
+    client.connect();
 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
+    // Make the appropriate DB calls
+    // await  listDatabases(client);
 
-        // Make the appropriate DB calls
-        await  listDatabases(client);
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+} catch (e) {
+    console.error(e);
+    
+} finally {
+    client.close();
 }
 
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
+function listDatabases(client){
+    databasesList = client.db().admin().listDatabases();
 
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
-
-main().catch(console.error);
