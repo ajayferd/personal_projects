1808ba0899069d92fd7edfb7807f96d770b0e42c
ajayferd <alejandro.fernandez@knights.ucf.edu>
2022-11-18 16:02:13 -0500

    files for php

---
+++ projects/CGS3175/website_frontend/Assignment3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <header>
+    <meta charset = "utf-8">
+    <title></title>
+    </header>
+    <nav></nav>
+    <section>
+
+    </section>
+    <?php include 'footer.php'?>
+</html>
---
+++ projects/CGS3175/website_frontend/footer.php
@@ -0,0 +1,1 @@
+<h2> This is from the footer php file <h2>
